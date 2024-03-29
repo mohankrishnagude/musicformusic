31c2bfe9b333243d545e16919478b2f89f61137a
Gude venkata Mohana Krishna <121798336+mohankrishnagude@users.noreply.github.com>
2024-03-29 16:07:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+import pickle
+import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Load pickle files
+try:
+    music = pickle.load(open('df.pkl', 'rb'))
+    similarity = pickle.load(open('similarity.pkl', 'rb'))
+except FileNotFoundError:
+    st.error("Pickle files not found. Please make sure the pickle files 'df.pkl' and 'similarity.pkl' exist.")
+    st.stop()  # Stop execution if pickle files are not found
+except Exception as e:
+    st.error(f"An error occurred while loading pickle files: {e}")
+    st.stop()  # Stop execution if an error occurs while loading pickle files
+
+# Initialize the Spotify client
+CLIENT_ID = "c7f9a9eb848b4022bae1fa67138f5109"
+CLIENT_SECRET = "2ca8e180612d444c9fddd4f2cf1455c8"
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Define functions to get song album cover URL and recommend songs
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+
+def recommend(song):
+    index = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:6]:
+        artist = music.iloc[i[0]].artist
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
+        recommended_music_names.append(music.iloc[i[0]].song)
+
+    return recommended_music_names, recommended_music_posters
+
+# Streamlit code
+st.header('Music Recommender System')
+
+music_list = music['song'].values
+selected_song = st.selectbox(
+    "Type or select a song from the dropdown",
+    music_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_music_names, recommended_music_posters = recommend(selected_song)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_music_names[0])
+        st.image(recommended_music_posters[0])
+    with col2:
+        st.text(recommended_music_names[1])
+        st.image(recommended_music_posters[1])
+    with col3:
+        st.text(recommended_music_names[2])
+        st.image(recommended_music_posters[2])
+    with col4:
+        st.text(recommended_music_names[3])
+        st.image(recommended_music_posters[3])
+    with col5:
+        st.text(recommended_music_names[4])
+        st.image(recommended_music_posters[4])
